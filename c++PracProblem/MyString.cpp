137e9584318f72b865ef96fe80b85c8627f5e757
hochang <love47024702@naver.com>
2021-10-28 19:27:00 +0900

    실습 6 - 제출 전 수정

--- c++PracProblem/MyString.cpp
+++ c++PracProblem/MyString.cpp
@@ -5,44 +5,54 @@ using namespace std;
 
 MyString::MyString()
 {
-	initialization();
-}
-
-void MyString::initialization()
-{
-	if (charArr != NULL)
-		delete charArr;
-
-	const int INIT_CAPACITY = 8;
+	charArr = new char[INIT_CAPACITY];
 	size = 0;
-	capacity = INIT_CAPACITY;
-	charArr = new char[capacity];
+	charArr[0] = '\0';
 }
 
 MyString::MyString(const char *srcStr)
 {
-	initialization();
-	int idx = 0;
-	while (srcStr[idx] != '\0')
-	{
-		addChar(srcStr[idx++]);
-	}
+	size = getNewArrSize(srcStr);
+	capacity = getNewArrCapacity(size);
+	charArr = new char[capacity];
+	copyCharArr(size, capacity, srcStr);
 }
 
 MyString::MyString(const MyString& srcStr)
 {
-	initialization();
-	copyCharArr(srcStr);
+	size = srcStr.size;
+	capacity = srcStr.capacity;
+	charArr = new char[srcStr.capacity];
+	copyCharArr(srcStr.size, srcStr.capacity, srcStr.charArr);
 
 }
 
-void MyString::addChar(char newChar)
+int MyString::getNewArrCapacity(int newSize)const
 {
-	if (size == capacity)
-		resize(2 * capacity);
-	charArr[size++] = newChar;
+	return (newSize / INIT_CAPACITY + 1) * INIT_CAPACITY;
 }
 
+int MyString::getNewArrSize(const char* srcStr)const
+{
+	int strLen;
+	for (strLen = 0; srcStr[strLen] != '\0'; strLen++);
+	return strLen;
+}
+
+void MyString::copyCharArr(int newSize, int newCapacity, const char* srcStr)
+{
+	if (capacity != newCapacity)
+	{
+		delete[] charArr;
+		charArr = new char[newCapacity];
+	}
+	
+	size = newSize;
+	capacity = newCapacity;
+	for (int i = 0; i < size; i++)
+		charArr[i] = srcStr[i];
+	charArr[size] = '\0';
+}
 
 void MyString::resize(int newCapacity)
 {
@@ -51,66 +61,77 @@ void MyString::resize(int newCapacity)
 	{
 		tempStr[i] = charArr[i];
 	}
+	tempStr[size] = '\0';
+
 	delete[] charArr;
 	charArr = tempStr;
 	capacity = newCapacity;
 }
 
-void MyString::copyCharArr(const MyString& srcStr)
+MyString::~MyString()
 {
-	capacity = srcStr.capacity;
-	size = srcStr.size;
 	delete[] charArr;
-	charArr = new char[capacity];
-	for (int i = 0; i < size; i++)
-		charArr[i] = srcStr.charArr[i];
 }
 
-MyString& MyString::operator =(const MyString &srcStr)
+const MyString& MyString::operator =(const MyString &srcStr)
 {
 	if (this == &srcStr)
 		return *this;
 
-	copyCharArr(srcStr);
+	copyCharArr(srcStr.size, srcStr.capacity, srcStr.charArr);
 	return *this;
 }
 
-MyString& MyString::operator =(const char *srcStr)
+const MyString& MyString::operator =(const char *srcStr)
 {
-	MyString tempMyString = srcStr;
-	operator =(tempMyString);
+	int newSize = getNewArrSize(srcStr);
+	copyCharArr(newSize, getNewArrCapacity(newSize), srcStr);
 	return 	*this;
 }
 
-const MyString MyString::operator +(const MyString& str2nd)
+const MyString MyString::plus(int str2ndSize, const char *str2nd)const
 {
-	MyString newString(*this);// 복사생성자
-	
-	for (int i = 0; i < str2nd.size; i++)
-		newString.addChar(str2nd.charArr[i]);
-	return MyString(newString);
+	MyString temp(charArr);
+	temp.appendMyString(str2ndSize,str2nd);
+	return temp;
 }
 
-const MyString MyString::operator +(const char *str2nd)
+const MyString MyString::operator +(const MyString& str2nd)const
 {
-	return operator +(MyString(str2nd));
+	return plus(str2nd.size, str2nd.charArr);
+}
+
+const MyString MyString::operator +(const char *str2nd)const
+{
+	int str2ndSize = getNewArrSize(str2nd);
+	return plus(str2ndSize, str2nd);
 }
 
 const MyString& MyString::append(const MyString& str2nd)
 {
-	MyString temp = operator +(str2nd);
-	copyCharArr(temp);
+	appendMyString(str2nd.size, str2nd.charArr);
 	return *this;
 }
 
 const MyString& MyString::append(const char* str2nd)
 {
-	return append(MyString(str2nd));
+	appendMyString(getNewArrSize(str2nd), str2nd);
+	return *this;
+	
 }
-
-MyString::~MyString()
+void MyString::appendMyString(int str2ndSize, const char* str2ndCharArr)
 {
-	delete[] charArr;
+	int newSize = size + str2ndSize;
+	if(getNewArrCapacity(newSize)>capacity)
+	{
+		resize(getNewArrCapacity(newSize));
+	}
+
+	for (int i = 0; i < str2ndSize; i++)
+		charArr[size + i] = str2ndCharArr[i];
+	charArr[newSize] = '\0';
+	size = newSize;
+
 }
 
 int MyString::length()const
@@ -131,8 +152,7 @@ bool  MyString::empty()const
 {
 	return size == 0;
 }
-
-bool MyString::operator ==(const MyString& str)
+bool MyString::isEqual(const MyString& str)const
 {
 	if (size == str.size)
 	{
@@ -145,23 +165,101 @@ bool MyString::operator ==(const MyString& str)
 	}
 	return false;
 }
-
-int MyString::find(const char* subStr)
+bool MyString::operator ==(const MyString& str)const
 {
-	return find(0,subStr);
+	return isEqual(str);
 }
 
-int MyString::find(const MyString subStr)
+bool MyString::operator !=(const MyString& str)const
 {
-	return find(0, subStr);
+	return !isEqual(str);
 }
 
-int MyString::find(int pos, const char* subStr)
+const MyString& MyString::operator +=(const char* str2nd)
 {
-	return find(pos, MyString(subStr));
+	appendMyString(getNewArrSize(str2nd), str2nd);
+	return *this;
+}
+bool MyString::greaterThan(int strSize, const char* srcStr)const
+{
+	int i = 0;
+	while (i < size && i < strSize)
+	{
+		if (charArr[i] > srcStr[i])
+			return true;
+		else if (charArr[i] < srcStr[i])
+			return false;
+		else
+			i++;
+	}
+	if (size > strSize)
+		return true;
+
+	return false;
 }
 
-int MyString::find(int pos, const MyString subStr)
+bool MyString::lessThan(int strSize, const char* srcStr)const
+{
+	int i = 0;
+	while (i < size && i < strSize)
+	{
+		if (charArr[i] < srcStr[i])
+			return true;
+		else if (charArr[i] > srcStr[i])
+			return false;
+		else
+			i++;
+	}
+	if (size < strSize)
+		return true;
+
+	return false;
+}
+
+bool MyString::operator <(const MyString& str)const
+{
+	return lessThan(str.size, charArr);
+}
+
+bool MyString::operator >(const MyString& str)const
+{
+	return greaterThan(str.size, charArr);
+
+}
+
+bool MyString::operator <=(const MyString& str)const
+{
+	return !greaterThan(str.size, charArr);
+
+}
+
+bool MyString::operator >=(const MyString& str)const
+{
+	return !lessThan(str.size, charArr);
+}
+
+bool MyString::operator <(const char* str)const
+{
+	return lessThan(getNewArrSize(str), str);
+}
+
+bool MyString::operator >(const char* str)const
+{
+	return greaterThan(getNewArrSize(str), str);
+
+}
+
+bool MyString::operator <=(const char* str)const
+{
+	return !greaterThan(getNewArrSize(str), str);
+}
+
+bool MyString::operator >=(const char* str)const
+{
+	return !lessThan(getNewArrSize(str), str);
+}
+
+int MyString::findPos(int pos, int subStrSize, const char* subStr)const
 {
 	int resultPos = -1;
 	int subStrIdx = 0;
@@ -170,177 +268,215 @@ int MyString::find(int pos, const MyString subStr)
 
 	for (int i = pos; i < size; i++)
 	{
-		if (charArr[i] == subStr.charArr[subStrIdx])
+		if (charArr[i] == subStr[subStrIdx])
 			subStrIdx++;
 		else
 		{
 			subStrIdx = 0;
-			if (charArr[i] == subStr.charArr[subStrIdx])
+			if (charArr[i] == subStr[subStrIdx])
 				subStrIdx++;
 		}
 
-		if (subStrIdx == subStr.size)
+		if (subStrIdx == subStrSize)
 		{
-			resultPos = i - subStr.size + 1;
+			resultPos = i - subStrSize + 1;
 			break;
 		}
 	}
 	return resultPos;
 }
 
-bool MyString::operator !=(const MyString& str)
+int MyString::find(const char* subStr)const
 {
-	return !operator==(str);
+	return findPos(0,getNewArrSize(subStr),subStr);
 }
 
-const MyString& MyString::operator +=(const char* str2nd)
+int MyString::find(const MyString subStr)const
 {
-	copyCharArr(operator +(str2nd));
-	return *this;
+	return findPos(0, subStr.size, subStr.charArr);
 }
 
-bool MyString::operator <(const MyString &str)const
+int MyString::find(int pos, const char* subStr)const
 {
-	int i = 0;
-	while (i < size && i < str.size)
-	{
-		if (charArr[i] < str.charArr[i])
-			return true;
-		else if (charArr[i] > str.charArr[i])
-			return false;
-		else
-			i++;
-	}
-	if (size < str.size)
-		return true;
+	return findPos(pos, getNewArrSize(subStr), subStr);
+}
 
-	return false;
+int MyString::find(int pos, const MyString subStr)const
+{
+	return findPos(pos, subStr.size, subStr.charArr);
 
 }
 
-bool MyString::operator >(const MyString& str)const
-{
-	int i = 0;
-	while (i < size && i < str.size)
-	{
-		if (charArr[i] > str.charArr[i])
-			return true;
-		else if (charArr[i] < str.charArr[i])
-			return false;
-		else
-			i++;
-	}
-	if (size > str.size)
-		return true;
-
-	return false;
-}
-
-bool MyString::operator <=(const MyString& str)const
-{
-	return !operator >(str);
-}
-
-bool MyString::operator >=(const MyString& str)const
-{
-	return !operator <(str);
-}
-
-bool MyString::operator <(const char* str)const
-{
-	return operator <(MyString(str));
-}
-
-bool MyString::operator >(const char* str)const
-{
-	return operator >(MyString(str));
-}
-
-bool MyString::operator <=(const char* str)const
-{
-	return operator <=(MyString(str));
-}
-
-bool MyString::operator >=(const char* str)const
-{
-	return operator >=(MyString(str));
-}
 
 const MyString MyString::substr(int pos, int cnt)const
 {
+	
+	char* tempChar;
+	int idx = 0;
+	if (pos + cnt - 1 < size)
+		tempChar = new char[cnt+1];
+	else
+		tempChar = new char[size-pos+1];
 
-	MyString tempStr;
 	for (int i = pos; (i < size) && (i < pos+cnt) ; i++)
 	{
-		tempStr.addChar(charArr[i]);
+		tempChar[idx++] = charArr[i];
+
 	}
-	return MyString(tempStr);
+	tempChar[idx] = '\0';
+
+	MyString tempStr(tempChar);
+	delete[] tempChar;
+
+	return tempStr;
 }
 
-const MyString& MyString::insert(int pos, const MyString& subStr) // -1넣었을때 어케 되나 확인해보기
+const MyString& MyString::erase(int pos, int cnt)//1,4
 {
-	MyString tempStr;
-	for (int i = 0; i < pos; i++)
-		tempStr.addChar(charArr[i]);
-	for (int i = 0; i < subStr.size; i++)
-		tempStr.addChar(subStr.charArr[i]);
-	for(int i = pos; i < size; i++)
-		tempStr.addChar(charArr[i]);
-	copyCharArr(tempStr);
-	return *this; //리턴 타입이 this인지 새로운 객체인지 생각해보기
-} 
+	if (pos < 0 || pos >= size|| cnt == 0)
+		return *this;
+
+	int idx;
+	for (idx = pos; (idx + cnt) < size; idx++)
+	{
+		charArr[idx] = charArr[idx+cnt];
+	}
+	charArr[idx] = '\0';
+	size = idx;
+	
+	int newCapacity = getNewArrCapacity(size);
+	if (newCapacity < capacity)
+		resize(newCapacity); 
+	return *this;
+}
+
+//여기부터 테스트하기
+
+void MyString::insertStr(int pos, int subStrSize, const char* subStr)
+{
+	if (pos < 0 || pos > size || subStrSize == 0)
+	{
+		return;
+	}
+		
+	int newSize = size + subStrSize;
+	int newCapacity = getNewArrCapacity(newSize);
+	if (capacity < newCapacity)
+		resize(newCapacity);
+
+	for (int i = size - 1; i >= pos; i--)
+		charArr[i + subStrSize] = charArr[i];
+	for (int i = 0; i < subStrSize; i++)
+		charArr[pos + i] = subStr[i];
+	size = newSize;
+	charArr[size] = '\0';
+
+}
+
+const MyString& MyString::insert(int pos, const MyString& subStr)
+{
+	insertStr(pos, subStr.size, subStr.charArr);
+
+	return *this;
+}
 
 const MyString& MyString::insert(int pos, const char* subStr)
 {
-	return insert(pos,MyString(subStr));
+	insertStr(pos, getNewArrSize(subStr), subStr);
+
+	return *this;
 }
 
+int stoi(const MyString& srcStr, int pos , int base)
+{
+	int result = 0, curNum = 0;
+	bool isNegative = false;
+	if (srcStr.charArr[pos] == '-'|| srcStr.charArr[pos] == '+')
+	{
+		if (srcStr.charArr[pos] == '-')
+			isNegative = true;
+		pos++;
+	}
+		
+	for(int i=pos; i< srcStr.size; i++)
+	{
+		if ('0'<= srcStr.charArr[i] && srcStr.charArr[i]<'0'+base && srcStr.charArr[i] <= '9')
+		{
+			curNum = srcStr.charArr[i] - '0';
+		}
+		else if(base>10&& ('A' <= toupper(srcStr.charArr[i])) && (toupper(srcStr.charArr[i]) < ('A' + (base % 10))))
+		{
+
+			curNum = 10 + (toupper(srcStr.charArr[i]) - 'A');
+				
+		}
+		else
+		{
+			break;
+		}
+		result = base * result + curNum;
+	}
+	
+	if (isNegative)
+		return -result;
+	return result;
+}
+
+
+char& MyString::operator [](int pos)
+{
+	return charArr[pos];
+}
+
+void MyString::replaceStr(int pos, int cnt, int subStrSize, const char* subStr)
+{
+	erase(pos, cnt);
+	insertStr(pos, getNewArrSize(subStr), subStr);
+}
+	
 const MyString& MyString::replace(int pos, int cnt, const MyString& subStr)
 {
-	MyString tempStr;
-	for (int i = 0; i < pos; i++)
-		tempStr.addChar(charArr[i]);
-	tempStr.append(subStr);
-
-	for(int i=pos+cnt;i<size;i++)
-		tempStr.addChar(charArr[i]);
-	copyCharArr(tempStr);
+	replaceStr(pos, cnt, subStr.size, subStr.charArr);
 	return *this;
 }
 
 const MyString& MyString::replace(int pos, int cnt, const char* subStr)
 {
-	return replace(pos, cnt, MyString(subStr));
+	replaceStr(pos, cnt, getNewArrSize(subStr), subStr);
+	return *this;
 }
 
-const MyString& MyString::erase(int pos, int cnt)
+void MyString::addChar(char c)
 {
-	return replace(pos, cnt, "");
+	int newCapacity = getNewArrCapacity(size + 1);
+	if (newCapacity > capacity)
+		resize(newCapacity);
+	charArr[size++] = c;
+	charArr[size] = '\0';
+
 }
 
 ostream& operator <<(ostream& os, const MyString &str)//테스트 종료 후 변경하기
 {
-	for (int i = 0; i < str.size; i++)
-		cout << str.charArr[i];
-	cout << " size:" << str.size << " capacity:" << str.capacity << "객체주소"<<&str<< endl; //테스트 휴 지우기
+	os << str.charArr<<";";
+	
+	os << "size:" << str.size << " capacity:" << str.capacity << "객체주소"<<&str<< endl; //테스트 휴 지우기
 	
 	return os;
 }
 
 istream& operator >>(istream& is, MyString &str)
 {
-	str.initialization();
-
-	while (1)
+	if (str.size != 0)
 	{
-		char newChar = getchar();
+		str.size = 0; str.capacity = str.INIT_CAPACITY; delete[] str.charArr;
+		str.charArr = new char[str.capacity];
+	}
 
-		if (newChar == '\n')
-			break;
-
-		if (str.size == str.capacity)
-			str.resize(2 * str.capacity);
-		str.charArr[str.size++] = newChar;
+	char inputChar;
+	for (int idx = 0; (inputChar = is.get()) != '\n'; idx++)
+	{
+		str.addChar(inputChar);
 	}
 
 	return is;
--- c++PracProblem/MyString.h
+++ c++PracProblem/MyString.h
@@ -9,18 +9,20 @@ public:
 	MyString(const MyString& srcStr);
 	~MyString();
 
-	MyString& operator =(const MyString &srcStr);
-	MyString& operator =(const char *srcStr);
-	const MyString operator +(const MyString& str2nd);
-	const MyString operator +(const char *str2nd);
+	const MyString& operator =(const MyString &srcStr);
+	const MyString& operator =(const char *srcStr);
+	const MyString operator +(const MyString& str2nd)const;
+	const MyString operator +(const char *str2nd)const;
 	const MyString& append(const char *str2nd);
 	const MyString& append(const MyString& str2nd);
-	bool operator ==(const MyString &str); //const 붙여야 되는거 아닌가?
-	bool operator !=(const MyString &str); //const 붙여야 되는거 아닌가?
-	int find(const char *subStr);
-	int find(const MyString subStr);
-	int find(int pos, const char *subStr);
-	int find(int pos, const MyString subStr);
+	bool operator ==(const MyString &str)const; 
+	bool operator !=(const MyString &str)const; 
+	const MyString& operator +=(const char* str2nd);
+
+	int find(const char *subStr)const;
+	int find(const MyString subStr)const;
+	int find(int pos, const char *subStr)const;
+	int find(int pos, const MyString subStr)const;
 
 	int length()const; 
 	char at(int pos)const;
@@ -29,7 +31,6 @@ public:
 	friend istream& operator >>(istream& is, MyString &str);
 	friend ostream& operator <<(ostream& os, const MyString &str);
 	
-	const MyString& operator +=(const char *str2nd);
 
 	bool operator <(const MyString &str)const;
 	bool operator >(const MyString &str)const;
@@ -41,20 +42,34 @@ public:
 	bool operator <=(const char* str)const;
 	bool operator >=(const char* str)const;
 
-	const MyString substr(int pos, int cnt)const; //앞에 const 맞나?
-	const MyString& insert(int pos, const MyString& subStr); //this넘겨주는게 맞나?
+	const MyString substr(int pos, int cnt)const; 
+	const MyString& insert(int pos, const MyString& subStr); 
 	const MyString& insert(int pos, const char *subStr);
 
 	const MyString& replace(int pos, int cnt,const MyString &subStr);
 	const MyString& replace(int pos, int cnt,const char *subStr);
 
 	const MyString& erase(int pos, int cnt);
+
+	char& operator [](int pos);
+	friend int stoi(const MyString &srcStr, int pos=0 , int base=10);
+
 private:
 	int size;
 	int capacity;
 	char *charArr=NULL;
+	static const int INIT_CAPACITY = 8;
 	void resize(int newCapacity);
-	void initialization();
-	void addChar(char newChar);
-	void copyCharArr(const MyString& srcStr);
+	int getNewArrCapacity(int newSize)const;
+	int getNewArrSize(const char* srcStr)const;
+	void copyCharArr(int newSize,int newCapacity, const char* srcStr);
+	const MyString plus(int str2ndSize, const char* str2nd)const;
+	void appendMyString(int str2ndSize, const char* str2ndCharArr);
+	bool isEqual(const MyString& str)const;
+	bool greaterThan(int strSize, const char* srcStr)const;
+	bool lessThan(int strSize, const char* srcStr)const;
+	int findPos(int pos, int subStrSize, const char* subStr)const;
+	void insertStr(int pos,int subStrSize, const char* subStr);
+	void replaceStr(int pos, int cnt, int subStrSize, const char* subStr);
+	void addChar(char c);
 };
