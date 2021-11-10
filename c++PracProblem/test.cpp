e646eb4dcad14e63bb70d5019421fc3d9496d325
hochang <love47024702@naver.com>
2021-11-10 20:25:31 +0900

    실습 - 7 상속,다형성 테스트

--- c++PracProblem/test.cpp
+++ c++PracProblem/test.cpp
@@ -1,18 +1,47 @@
+#include <fstream>
 #include <iostream>
+#include "Person.h"
+#include "Student.h"
+#include "Staff.h"
+#include "GeneralPerson.h"
 using namespace std;
-int getday(int year, int month, int day);
-bool isValidMonthDay(int year, int month, int day);
 
-int main() 
+void fileInputAndClose();
+
+
+int main()
 {
-	int arr[3] = { 1,2,3 }, arr2[3];
-	for (int i = 0; i < 3; i++)
-	{
-		arr2[i] = arr[i];
-	}
-	arr2[0] = 5;
-	cout << arr[0] << arr2[0] << endl;
 
+	
+		Person* ptrPerson[4];
+		ptrPerson[0] = new Student("3", "성호창", "컴소공", "010-8434-4855", 123);
+		ptrPerson[1] = new Staff("2", "성호창", "컴소공", "010-9456-9451", "0473", 123);
+		ptrPerson[2] = new GeneralPerson("V01111", "성1", "010-5412-451",12);
+		ptrPerson[3] = new GeneralPerson("성호창1", "010-5412-451", 43);
+
+		for (int i = 0; i < 4; i++) {
+			ptrPerson[i]->print();
+			cout << endl;
+			cout << ptrPerson[i]->toString() << endl;
+		}
+
+		cout << (*ptrPerson[0] > *ptrPerson[1]) << endl;
 
 }
+
+void fileInputAndClose()//파일 입출력
+{
+
+	ifstream inStream;
+	ofstream outStream;
+	string line;
+	inStream.open("FundList.txt");
+	outStream.open("outFile.txt");
+	for (int i = 0; !inStream.eof(); i++) {
+		getline(inStream, line);
+		cout << line << endl;
+		outStream << line << endl;
+	}
+}
+
 
